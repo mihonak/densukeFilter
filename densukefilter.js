7b1d27c87ab52220a581b4664b45352bb75f7a45
Miho Nakayama <miho.nakayam@gmail.com>
2015-07-28 18:45:46 +0900

    refactor

--- densukefilter.js
+++ densukefilter.js
@@ -2,39 +2,39 @@
 	s = document.createElement("script");
 	s.src = "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";
 	s.onload = function() {
-		f(jQuery.noConflict(true))
+		f(jQuery.noConflict(true));
 	};
-	document.body.appendChild(s)
+	document.body.appendChild(s);
 })(function($){
-	$('.regist tr:gt(0) td:nth-child(2), .regist tr:gt(0) td:nth-child(3), .regist tr:gt(0) td:nth-child(4)').hover(function(){
-			$(this).css('background','#ecc');
+	$('<style type="text/css">td.hover{background:#ff0 !important;}</style>').appendTo('head');
+	var $table = $('div.regist').children('table'),
+		$statusCols = $table.find('tr:gt(0)').find('td:nth-child(2), td:nth-child(3), td:nth-child(4)'),
+		$td = $('td');
+	$statusCols.hover(function(){
+			$(this).addClass('hover');
 		},function(){
-			$(this).css('background','#fff');
-		});
-	$('.regist tr:gt(0) td:nth-child(2)').click(function(){
-		densukeFilter($(this).closest('tr').index(),'ok');
+			$(this).removeClass('hover');
 	});
-	$('.regist tr:gt(0) td:nth-child(3)').click(function(){
-		densukeFilter($(this).closest('tr').index(),'mb');
-	});
-	$('.regist tr:gt(0) td:nth-child(4)').click(function(){
-		densukeFilter($(this).closest('tr').index(),'ng');
-	});
-	function densukeFilter(row,status){
-		$('td').show();
-		var blanks = [],
+	$statusCols.click(function(){
+		var row = $(this).closest('tr').index(),
+			status = $(this).index(),
+			blanks = [],
 			statusClass;
-		if(status=='ok') statusClass = '.col3';
-		if(status=='mb') statusClass = '.col2';
-		if(status=='ng') statusClass = '.col1';
-		$('.regist tr:eq('+row+') td:gt(3):not(:last-child)').each(function(index,elm){
-			if($(elm).has(statusClass).length==0){
+		$td.show();
+		switch (status){
+			case 1 : statusClass = '.col3'; break;
+			case 2 : statusClass = '.col2'; break;
+			case 3 : statusClass = '.col1'; break;
+			default : break;
+		}
+		$table.find('tr:eq('+ row +') td:gt(3)').each(function(index,elm){
+			if($(elm).has(statusClass).length == 0){
 				blanks.push(index);
 			}
 		});
-		for(var i=0; i<blanks.length; i++){
+		for(var i = 0; i < blanks.length; i++){
 			var num = blanks[i] + 5;
-			$('.regist td:nth-child('+ num +')').hide();
+			$table.find('td:nth-child('+ num +')').hide();
 		}
-	}
+	});
 });
